a7b4c3ab664a82273e3910af8a075862022026a2
mellisa <mellisamugiyo1@gmail.com>
2023-06-08 11:26:49 +0200

    included a block of if staments

---
+++ file.py
@@ -0,0 +1,9 @@
+name='mellisa'
+mood='trilled'
+school='hyperiondev'
+print(name ,'is',mood,"to be at",school)
+if name=='mellisa':
+    print('ok, get ready to build a robot with mellisa')
+
+else:
+    print('your name is not mellisa')
